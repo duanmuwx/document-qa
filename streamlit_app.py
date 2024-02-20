be2ddedad30b5ccee290b5c361628a1b429a366f
duanmu_weixiang <duanmuweixiang@163.com>
2024-02-20 12:44:54 +0800

    mutilple pdf files

--- streamlit_app.py
+++ streamlit_app.py
@@ -380,10 +380,10 @@ if uploaded_file and not st.session_state.loaded_embeddings:
 
     with right_column:
         with st.spinner('Reading file, calling Grobid, and creating memory embeddings...'):
-            binary = uploaded_file.getvalue()
-            tmp_file = NamedTemporaryFile()
-            tmp_file.write(bytearray(binary))
-            st.session_state['binary'] = binary
+            # binary = uploaded_file.getvalue()
+            # tmp_file = NamedTemporaryFile()
+            # tmp_file.write(bytearray(binary))
+            # st.session_state['binary'] = binary
 
             st.session_state['doc_id'] = hash = st.session_state['rqa'][model].process_uploaded_files(uploaded_file,
                                                                                                       chunk_size=chunk_size,
